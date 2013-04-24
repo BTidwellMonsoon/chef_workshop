0adb73e713c780deb1bd621ddbac35ed03a80f25
Blake Tidwell <blake@monsoonco.com>
2013-04-24 12:18:11 -0700

    Moved over default recipe from gist and modified Vagrantfile to pass basic tests

--- myface/files/default/tests/minitest/default_test.rb
+++ myface/files/default/tests/minitest/default_test.rb
@@ -5,7 +5,8 @@ describe 'myface::default' do
   include Helpers::Myface
 
   # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
+  it 'creates a service account for the myface daemon' do
+    user("myface").must_exist
   end
 
 end
--- myface/recipes/default.rb
+++ myface/recipes/default.rb
@@ -2,7 +2,38 @@
 # Cookbook Name:: myface
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
+# Copyright (C) 2013 Blake Tidwell
 # 
 # All rights reserved - Do Not Redistribute
 #
+user "myface"
+
+include_recipe "mysql::server"
+# #Include the mysql Ruby bindings for Chef
+include_recipe "mysql::ruby"
+
+# Create mysql connection binding
+# mysql_connection_info = {:host => "localhost",
+#                          :username => 'root',
+#                          :password => node['mysql']['server_root_password']}
+
+# # Metadata dependency on the database cookbook provides
+# # access to the mysql_database provider.
+# mysql_database 'myface' do
+#   connection mysql_connection_info
+#     action :create
+# end
+
+# Write schema seed file to filesystem
+# cookbook_file "/tmp/myface-init.sql" do
+#     source "myface-init.sql"
+#     owner "root"
+#     group "root"
+#     mode "0644"
+# end
+
+# # Seed the database with test data
+# execute "initialize myface database" do
+#   command "mysql -h localhost -u root -p#{node['mysql']['server_root_password']} -D myface < /tmp/myface-init.sql"
+#   not_if "mysql -h localhost -u root -p#{node['mysql']['server_root_password']} -D myface -e 'describe users;'"
+# end
